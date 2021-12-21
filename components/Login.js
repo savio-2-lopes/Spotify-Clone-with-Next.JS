28b5783e1fd20ddf526d5a4f1a01cf2fa39e9f41
savio-2-lopes <savioaugulopes@gmail.com>
2021-12-20 23:37:02 -0300

    :tada: Add project

--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { getProviders, signIn } from "next-auth/react";
 
-function Login() {
+function Login({ providers }) {
   return (
-    <div>
-      <h1>THis is Login Page</h1>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img src="https://" alt="Logo Spotify" className="w-52 mb-5" />
+      {Object.values(providers).map((provider) => {
+        <div key={provider.name}>
+          <button
+            onClick={() =>
+              signIn(provider.id, {
+                callbackUrl: "/",
+              })
+            }
+            className="bg-#18D860 text-white p-5 rounded-full"
+          >
+            Login with {provider.name}
+          </button>
+        </div>;
+      })}
     </div>
   );
 }
 
 export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
