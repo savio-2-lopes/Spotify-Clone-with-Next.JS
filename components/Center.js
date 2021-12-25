3105641b4d34e478b9a3efff397254811be9c6f2
unknown <savioaugulopes@gmail.com>
2021-12-25 17:06:00 -0300

    Refactor app and fix bug

--- components/Center.js
+++ components/Center.js
@@ -1,13 +1,29 @@
 import { useSession } from "next-auth/react";
 import { ChevronDownIcon } from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
+import { shuffle } from "lodash";
+
+const colors = [
+  "from-indigo-500",
+  "from-blue-500",
+  "from-green-500",
+  "from-yellow-500",
+  "from-pink-500",
+  "from-purple-500",
+];
 
 function Center() {
   const { data: session } = useSession();
+  const [color, setColor] = useState(null);
+
+  useEffect(() => {
+    setColor(shuffle(colors).pop());
+  }, []);
 
   return (
-    <div className="flex flex-grow">
-      <header>
-        <div className="flex items-center text-white space-x-3 opacity-90 hover:opacity-80">
+    <div className="flex-grow">
+      <header className="absolute top-5 right-8">
+        <div className="flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
           <img
             className="rounded-full w-10 h-10"
             src={session?.user.image}
@@ -17,6 +33,12 @@ function Center() {
           <ChevronDownIcon className="h-5 w-5" />
         </div>
       </header>
+
+      <section
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8`}
+      >
+        <h1>Add content center file</h1>
+      </section>
     </div>
   );
 }
