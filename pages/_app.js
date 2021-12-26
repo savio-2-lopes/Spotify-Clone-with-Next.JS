5fab278289a97a1bb05e2929635d7f2766f226b0
savio-2-lopes <savioaugulopes@gmail.com>
2021-12-26 01:19:07 -0300

    Refactor app and fix bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 import { RecoilRoot } from "recoil";
@@ -5,6 +6,15 @@ import { RecoilRoot } from "recoil";
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <title>Spotify Clone with Next.js</title>
+
+        <link rel="icon" type="image/png" href="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Spotify-256.png" />
+        {/* Image from: https://www.iconfinder.com/icons/1298766/spotify_music_sound_icon */}
+      </Head>
+      
       <RecoilRoot>
         <Component {...pageProps} />
       </RecoilRoot>
--- pages/login.js
+++ pages/login.js
@@ -4,10 +4,12 @@ function Login({ providers }) {
   return (
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
       <img
-        src="https://links.papareact.com/9xl"
+        src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Spotify-512.png"
         className="w-52 mb-5"
         alt="Logo"
       />
+      
+      {/* Image from: https://www.iconfinder.com/icons/1298766/spotify_music_sound_icon */}
 
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
