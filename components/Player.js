b227d8e64e260be7c24d020d0b8205d95a3445fe
savio-2-lopes <savioaugulopes@gmail.com>
2021-12-27 00:50:50 -0300

    Refactor app and fix bug

--- components/Player.js
+++ components/Player.js
@@ -34,10 +34,11 @@ function Player() {
   const fetchCurrentSong = () => {
     if (!songInfo) {
       spotifyApi.getMyCurrentPlayingTrack().then((data) => {
-        setCurrentIdTrack(data.body?.is_playing);
+        console.log("Now playing: ", data.body?.item);
+        setCurrentIdTrack(data.body?.item?.id);
 
         spotifyApi.getMyCurrentPlaybackState().then((data) => {
-          setIsPlaying(data.body?.isPlayingState);
+          setIsPlaying(data.body?.isPlaying);
         });
       });
     }
@@ -45,7 +46,7 @@ function Player() {
 
   const handlePlayPause = () => {
     spotifyApi.getMyCurrentPlaybackState().then((data) => {
-      if (data.body.is_playing) {
+      if (data.body.isPlaying) {
         spotifyApi.pause();
         setIsPlaying(false);
       } else {
@@ -60,7 +61,7 @@ function Player() {
       fetchCurrentSong();
       setVolume(50);
     }
-  }, [currentTrackIdState, spotifyApi, session]);
+  }, [currentTrackId, spotifyApi, session]);
 
   useEffect(() => {
     if (volume > 0 && volume < 100) {
@@ -78,38 +79,32 @@ function Player() {
   return (
     <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
       {/* Left */}
-
       <div className="flex items-center space-x-4">
         <img
           className="hidden md:inline h-10 w-10"
           src={songInfo?.album.images?.[0]?.url}
-          alt=""
+          alt="Playlist miniplay"
         />
+        <div>
+          <h3>{songInfo?.name}</h3>
+          <p>{songInfo?.artists?.[0]?.name}</p>
+        </div>
       </div>
-
-      {/* Center  */}
-
+      {/* Center */}
       <div className="flex items-center justify-evenly">
         <SwitchHorizontalIcon className="button" />
         <RewindIcon className="button" />
-
         {isPlaying ? (
           <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
         ) : (
           <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
         )}
-
-        <FastForwardIcon
-          // onClick={() => spotifyApi.skipToNext()} - The API is not working
-          className="button"
-        />
+        <FastForwardIcon className="button" />
         <ReplyIcon className="button" />
       </div>
-
       {/* Right */}
-
       <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
-        <VolumeDownIcon
+        <VolumeUpIcon
           onClick={() => volume > 0 && setVolume(volume - 10)}
           className="button"
         />
@@ -122,12 +117,11 @@ function Player() {
           max={100}
         />
         <VolumeUpIcon
-          onClick={() => volume < 100 && setVolume(volume + 100)}
+          onClick={() => volume < 100 && setVolume(volume + 10)}
           className="button"
         />
       </div>
     </div>
   );
 }
-
 export default Player;
